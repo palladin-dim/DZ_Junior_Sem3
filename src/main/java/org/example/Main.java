401e437fca991f912c8fac2377d276a8e2b15850
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2024-09-04 20:53:55 +0600

    DZ_Sem3.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+/**
+ * Задание 1: Создайте класс Person с полями name и age. Реализуйте сериализация и десериализацию этого класса в файл.
+ * <p></p>
+ *
+ */
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("Дмитрий", 45);
+
+        person.serialize("person.ser");
+
+        Person deserializedPerson = Person.deserialize("person.ser");
+
+        System.out.println("Имя: " + deserializedPerson.getName());
+        System.out.println("Возраст: " + deserializedPerson.getAge());
     }
 }
